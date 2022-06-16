0a5de0cd0861f30aa9458a87a5c3bf2237ebf749
VinaKashyap <106421827+VinaKashyap@users.noreply.github.com>
2022-06-16 20:44:24 +0530

    Update token.php

--- token.php
+++ token.php
@@ -1,6 +1,6 @@
 <?php
 $creds = json_decode(file_get_contents('assets/data/creds.json') , true);
-$ssoToken = $creds['ssoToken'];
+$ssoToken = $creds['eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1bmlxdWUiOiJkNjNjYjYyYy1lYTc0LTQzMzUtOWVmZS1hNjRkNzBkNjE2MDMiLCJ1c2VyVHlwZSI6IlJJTHBlcnNvbiIsImF1dGhMZXZlbCI6IjQwIiwiZGV2aWNlSWQiOiI4ODAyNjZhY2I1NDcyYmJjYWYwZTY0ZWE2YmE0NWE2ZDM1ZTAxNzVkZWQ2NTk4OTIwNGM0NDRhNzlhN2U3MWM4MDA0YWVkNTJlMDgyYWYzN2JkYjk1NGJiNTYxYjM3YmMxOWViOWMzNWMwZmI2MWFjOWVlZjE2ZjM3OTMwOWExNiIsImp0aSI6IjdiMzI4ZTc4LWViNGMtNGU0OC04MDk4LWU4YTJmMjhjYTYzZSIsImlhdCI6MTY1NTM5MjM5M30.AIPxTE0nKdU12AkCbIZOEbLK3PbwRg9rto1diDljg1o'];
 
 $jctBase = "cutibeau2ic";
 
